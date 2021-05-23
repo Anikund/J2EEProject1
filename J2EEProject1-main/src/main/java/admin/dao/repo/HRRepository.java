ef13e49581d10a3d161c583e7cc4eb47c35864f4
Anikund <56402239+Anikund@users.noreply.github.com>
2021-05-23 23:46:30 +0800

    Delete J2EEProject1-main directory
    Seems Duplicated

--- J2EEProject1-main/src/main/java/admin/dao/repo/HRRepository.java
+++
@@ -1,8 +0,0 @@
-package admin.dao.repo;
-
-import admin.dao.domain.HR;
-import org.springframework.data.repository.CrudRepository;
-
-public interface HRRepository extends CrudRepository<HR, Long> {
-    HR findByUsername(String username);
-}
