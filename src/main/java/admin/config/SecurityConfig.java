0806f85488461940f048a9118593c2c6660a6f86
zengyongqin <1973972996@qq.com>
2021-06-02 12:59:39 +0800

    1

--- src/main/java/admin/config/SecurityConfig.java
+++ src/main/java/admin/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package admin.config;
 
 import admin.service.HRRepositoryUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -16,7 +17,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private UserDetailsService userDetailsService;
     @Autowired
-    public void setUserDetailsService(UserDetailsService userDetailsService){
+    public void setUserDetailsService(@Qualifier("HRRepositoryUserDetailsService") UserDetailsService userDetailsService){
         this.userDetailsService = userDetailsService;
     }
 
